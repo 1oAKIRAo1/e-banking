3eafbae5c16c2749b0c899ea210186fe5797fd95
faisal <faisal@gmail.com>
2025-12-25 14:38:36 +0100

    projet

---
+++ ebank-backend/src/main/java/com/ebank/config/EmailConfig.java
@@ -0,0 +1,13 @@
+package com.ebank.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
